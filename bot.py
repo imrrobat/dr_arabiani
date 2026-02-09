4c1f53132736f2567c73ab2b16f8bb8e4a394de9
imrrobat <imrrobat@gmail.com>
2026-02-09 15:36:51 +0330

    admin

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import asyncio
 from aiogram import Bot, Dispatcher
 from aiogram.types import Message
 from aiogram.filters import CommandStart, Command
-from config import API_TOKEN
+from config import API_TOKEN, ADMIN_USER
 
 
 async def start_handler(pm: Message):
@@ -10,7 +10,10 @@ async def start_handler(pm: Message):
 
 
 async def add_handler(pm: Message):
-    await pm.answer("برای اضافه کردن باید ادمین باشین")
+    if pm.from_user.id == ADMIN_USER:
+        await pm.answer("شما ادمین هستین")
+    else:
+        await pm.answer("برای اضافه کردن باید ادمین باشین")
 
 
 async def main():
